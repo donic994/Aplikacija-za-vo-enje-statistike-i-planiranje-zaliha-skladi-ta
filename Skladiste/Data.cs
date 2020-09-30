56a8d30e73f5770c89ccadb26c68a63bee341287
Filip Gatarić <filip1310@hotmail.com>
2020-09-30 15:26:14 +0200

    Add files via upload

---
+++ Skladiste/Data.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skladiste
+{
+    class Data
+    {
+
+        public static List<string> getItems()
+        {
+            List<string> itemList = new List<string>();
+
+            itemList.Add("Čips");
+            itemList.Add("Krumpir");
+            itemList.Add("Luk");
+            itemList.Add("Pepsi");
+            itemList.Add("Sprite");
+            itemList.Add("Fanta");
+            itemList.Add("Zubna pasta");
+            itemList.Add("Sapun");
+            itemList.Add("Salama");
+            itemList.Add("Sir");
+            itemList.Add("Pašteta");
+            itemList.Add("Tuna");
+            itemList.Add("Mesni narezak");
+            itemList.Add("Pile");
+            itemList.Add("Salata");
+            itemList.Add("Ketchup");
+            itemList.Add("Majoneza");
+            itemList.Add("Ljuti umak");
+            itemList.Add("Kokošja juha");
+            itemList.Add("Alpska juha");
+            itemList.Add("Kiseli krastavci");
+            itemList.Add("Maska za lice");
+            itemList.Add("Sredstvo za dezinfekciju");
+            itemList.Add("Rukavice");
+            itemList.Add("Kruškovac");
+            itemList.Add("Crno vino");
+            itemList.Add("Bijelo vino");
+
+            return itemList;
+        }
+
+
+        public static List<string> getNames()
+        {
+            List<string> nameList = new List<string>();
+
+            nameList.Add("Filip");
+            nameList.Add("Jakov");
+            nameList.Add("Luka");
+            nameList.Add("Agata");
+            nameList.Add("Katarina");
+            nameList.Add("Valentina");
+            nameList.Add("Josip");
+            nameList.Add("Fran");
+            nameList.Add("Marko");
+            nameList.Add("Matija");
+            nameList.Add("David");
+            nameList.Add("Larisa");
+            nameList.Add("Ramona");
+            nameList.Add("Borna");
+            nameList.Add("Bruno");
+            nameList.Add("Tin");
+            nameList.Add("Klaudia");
+            nameList.Add("Veronika");
+            nameList.Add("Tena");
+            nameList.Add("Izabela");
+            nameList.Add("Hana");
+            nameList.Add("Karolina");
+            nameList.Add("Dražen");
+            nameList.Add("Sofija");
+            nameList.Add("Ines");
+            nameList.Add("Krunoslav");
+            nameList.Add("Krešimir");
+
+            return nameList;
+        }
+
+
+        public static int getQuantity()
+        {
+            Random r = new Random();
+            int quantity = r.Next(100, 800);
+
+            return quantity;
+        }
+
+        public static int getMinimum()
+        {
+            Random r = new Random();
+            int minimum = r.Next(0, 100);
+
+            return minimum;
+        }
+
+        public static int getMaximum()
+        {
+            Random r = new Random();
+            int maximum = r.Next(800, 1000);
+
+            return maximum;
+        }
+
+        public static int getExpiration()
+        {
+            Random r = new Random();
+            int expiration = r.Next(0, 1095); //1095 days = 3 years
+
+            return expiration;
+        }
+
+        public static string getItem()
+        {
+            Random r = new Random();
+            int index = r.Next(0, getItems().Count);
+
+            return getItems()[index];
+        }
+
+        public static string getName()
+        {
+            Random r = new Random();
+            int index = r.Next(0, getNames().Count);
+
+            return getNames()[index];
+        }
+
+    }
+}
---
+++ Skladiste/Database.cs
@@ -0,0 +1,391 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace Skladiste
+{
+    class Database
+    {
+        public static string infoMessage = "";
+        private static int delay = 300;
+
+        public static NpgsqlConnection GetConenction()
+        {
+            string server, username, password, database;
+            int port;
+
+            server = "127.0.1.1";
+            port = 5432;
+            username = "postgres";
+            password = "admin";
+            database = "postgres";
+
+            string connString = String.Format("Server={0};Port={1};" +
+                   "User Id={2};Password={3};Database={4};",
+                   server, port, username, password, database);
+
+            NpgsqlConnection connection = new NpgsqlConnection(connString);
+            connection.Open();
+
+            return connection;
+        }
+
+
+        public static void ExecuteSQL(string query, NpgsqlConnection con)
+        {
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+        }
+
+        public static void insertKupacRand(NpgsqlConnection con)
+        {
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            string sql = "INSERT INTO kupac(id, ime, artikl, kolicina) " +
+                         "VALUES(default, '" + Data.getName() + "', '" + Data.getItem() + "', " + Data.getQuantity() + ")";
+
+            Console.WriteLine(sql);
+            infoMessage = sql; 
+            try
+            {
+                Database.ExecuteSQL(sql, con);
+            }
+            catch (NpgsqlException npqe)
+            {
+                MessageBox.Show(npqe + "\nError on query:" + sql, "ERROR");
+            }
+            Thread.Sleep(delay);
+        }
+
+        public static void insertKupac(NpgsqlConnection con, string ime, string artikl, int kolicina)
+        {
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            string sql = "INSERT INTO kupac(id, ime, artikl, kolicina) " +
+                         "VALUES(default, '" + ime + "', '" + artikl+ "', " + kolicina + ")";
+
+            Console.WriteLine(sql);
+            infoMessage = sql;
+            try
+            {
+                Database.ExecuteSQL(sql, con);
+            }
+            catch (NpgsqlException npqe)
+            {
+                MessageBox.Show(npqe + "\nError on query:" + sql, "ERROR");
+            }
+            Thread.Sleep(delay);
+        }
+
+        public static void updateKupac(NpgsqlConnection con, string ime, string artikl, int kolicina, int id)
+        {
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            string sql = "UPDATE kupac set ime='" + ime + "', artikl='" + artikl + "', kolicina=" + kolicina + " where id=" + id;
+
+            Console.WriteLine(sql);
+            infoMessage = sql;
+            try
+            {
+                Database.ExecuteSQL(sql, con);
+            }
+            catch (NpgsqlException npqe)
+            {
+                MessageBox.Show(npqe + "\nError on query:" + sql, "ERROR");
+            }
+            Thread.Sleep(delay);
+        }
+
+        public static void deleteKupacAll(NpgsqlConnection con)
+        {
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            string sql = "DELETE FROM kupac";
+
+            try
+            {
+                Database.ExecuteSQL(sql, con);
+            }
+            catch (NpgsqlException npqe)
+            {
+                MessageBox.Show(npqe + "\nError on query:" + sql, "ERROR");
+            }
+            Thread.Sleep(delay);
+        }
+
+        public static void deleteKupac(NpgsqlConnection con, int id)
+        {
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            string sql = "DELETE FROM kupac where id=" +id;
+            infoMessage = sql;
+            try
+            {
+                Database.ExecuteSQL(sql, con);
+            }
+            catch (NpgsqlException npqe)
+            {
+                MessageBox.Show(npqe + "\nError on query:" + sql, "ERROR");
+            }
+            Thread.Sleep(delay);
+        }
+
+        public static DataTable selectKupacInProces()
+        {
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            NpgsqlConnection con = Database.GetConenction();
+
+            string sql = "SELECT * FROM kupac WHERE procesiranje=1";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, con);
+
+            ds.Reset();
+            da.Fill(ds);
+            dt = ds.Tables[0];
+
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            try
+            {
+                Database.ExecuteSQL(sql, con);
+            }
+            catch (NpgsqlException npqe)
+            {
+                MessageBox.Show(npqe + "\nError on query:" + sql, "ERROR");
+            }
+            Thread.Sleep(delay);
+
+            return dt;
+        }
+
+        public static DataTable selectKupacProcessed()
+        {
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            NpgsqlConnection con = Database.GetConenction();
+
+            string sql = "SELECT * FROM kupac WHERE procesiranje=2";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, con);
+
+            ds.Reset();
+            da.Fill(ds);
+            dt = ds.Tables[0];
+
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            try
+            {
+                Database.ExecuteSQL(sql, con);
+            }
+            catch (NpgsqlException npqe)
+            {
+                MessageBox.Show(npqe + "\nError on query:" + sql, "ERROR");
+            }
+            Thread.Sleep(delay);
+
+            return dt;
+        }
+
+
+        public static void insertSkladisteRand(NpgsqlConnection con)
+        {
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            string sql = "INSERT INTO skladiste(id, artikl, kolicina,  min_kolicina, max_kolicina, datum_dolaska, rok_trajanja) " +
+                         "VALUES(default, '" + Data.getItem() + "', " + Data.getQuantity() + " , " +
+                         Data.getMinimum() + " , " + Data.getMaximum() + " , '" + DateTime.Now + "', '"+
+                         DateTime.Now.AddDays(Data.getExpiration()) + "')";
+
+            Console.WriteLine(sql);
+            infoMessage = sql;
+            try
+            {
+                Database.ExecuteSQL(sql, con);
+            }
+            catch (NpgsqlException npqe)
+            {
+                MessageBox.Show(npqe + "\nError on query:" + sql, "ERROR");
+            }
+            Thread.Sleep(delay);
+        }
+
+        public static void insertSkladiste(NpgsqlConnection con, string artikl, int kolicina, int min, int max, DateTime rok)
+        {
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            string sql = "INSERT INTO skladiste(id, artikl, kolicina,  min_kolicina, max_kolicina, datum_dolaska, rok_trajanja) " +
+                         "VALUES(default, '" + artikl + "', " + kolicina + " , " +
+                         min+ " , " + max + " , '" + DateTime.Now + "', '" +
+                         rok + "')";
+
+            Console.WriteLine(sql);
+            infoMessage = sql;
+            try
+            {
+                Database.ExecuteSQL(sql, con);
+            }
+            catch (NpgsqlException npqe)
+            {
+                MessageBox.Show(npqe + "\nError on query:" + sql, "ERROR");
+            }
+            Thread.Sleep(delay);
+        }
+
+        public static void deleteSkladisteAll(NpgsqlConnection con)
+        {
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            string sql = "DELETE FROM skladiste";
+            infoMessage = sql;
+            try
+            {
+                Database.ExecuteSQL(sql, con);
+            }
+            catch (NpgsqlException npqe)
+            {
+                MessageBox.Show(npqe + "\nError on query:" + sql, "ERROR");
+            }
+            Thread.Sleep(delay);
+        }
+
+        public static void deleteSkladiste(NpgsqlConnection con, int id)
+        {
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            string sql = "DELETE FROM skladiste where id=" + id;
+            infoMessage = sql;
+            try
+            {
+                Database.ExecuteSQL(sql, con);
+            }
+            catch (NpgsqlException npqe)
+            {
+                MessageBox.Show(npqe + "\nError on query:" + sql, "ERROR");
+            }
+            Thread.Sleep(delay);
+        }
+
+        public static void updateSkladiste(NpgsqlConnection con, string artikl, int kolicina, int min, int max, int id, DateTime rok)
+        {
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            string sql = "UPDATE skladiste set artikl='" + artikl + "', kolicina=" + kolicina + " , min_kolicina=" +
+                         min + " , max_kolicina=" + max + " , rok_trajanja=" + rok + " where id=" + id;
+
+            Console.WriteLine(sql);
+            infoMessage = sql;
+            try
+            {
+                Database.ExecuteSQL(sql, con);
+            }
+            catch (NpgsqlException npqe)
+            {
+                MessageBox.Show(npqe + "\nError on query:" + sql, "ERROR");
+            }
+            Thread.Sleep(delay);
+        }
+
+        public static void deleteNarudzba(NpgsqlConnection con, int id)
+        {
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            string sql = "DELETE FROM narudzba where id=" + id;
+            infoMessage = sql;
+            try
+            {
+                Database.ExecuteSQL(sql, con);
+            }
+            catch (NpgsqlException npqe)
+            {
+                MessageBox.Show(npqe + "\nError on query:" + sql, "ERROR");
+            }
+            Thread.Sleep(delay);
+        }
+
+        public static void deleteNarudzbaAll(NpgsqlConnection con)
+        {
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            string sql = "DELETE FROM narudzba";
+            infoMessage = sql;
+            try
+            {
+                Database.ExecuteSQL(sql, con);
+            }
+            catch (NpgsqlException npqe)
+            {
+                MessageBox.Show(npqe + "\nError on query:" + sql, "ERROR");
+            }
+            Thread.Sleep(delay);
+        }
+        public static void deleteVelenarudzba(NpgsqlConnection con, int id)
+        {
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            string sql = "DELETE FROM velenarudzba where id=" + id;
+            infoMessage = sql;
+            try
+            {
+                Database.ExecuteSQL(sql, con);
+            }
+            catch (NpgsqlException npqe)
+            {
+                MessageBox.Show(npqe + "\nError on query:" + sql, "ERROR");
+            }
+            Thread.Sleep(delay);
+        }
+
+        public static void deleteVelenarudzbaAll(NpgsqlConnection con)
+        {
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            string sql = "DELETE FROM velenarudzba";
+            infoMessage = sql;
+            try
+            {
+                Database.ExecuteSQL(sql, con);
+            }
+            catch (NpgsqlException npqe)
+            {
+                MessageBox.Show(npqe + "\nError on query:" + sql, "ERROR");
+            }
+            Thread.Sleep(delay);
+        }
+
+        public static void deleteAll()
+        {
+            NpgsqlConnection con = Database.GetConenction();
+            deleteNarudzbaAll(con);
+            deleteVelenarudzbaAll(con);
+            deleteSkladisteAll(con);
+            deleteKupacAll(con);            
+            con.Close();
+        }
+
+    }
+}
